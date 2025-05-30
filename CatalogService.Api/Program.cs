30c261108344c778f0ffd37c35ad2680befabc25
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-05-30 15:48:13 +0300

    Add IRepository<Product> in DI

--- CatalogService.Api/Program.cs
+++ CatalogService.Api/Program.cs
@@ -1,5 +1,9 @@
+using CatalogService.Application.Contracts;
+using CatalogService.Application.Services;
 using CatalogService.Domain;
 using CatalogService.Infrastructure;
+using CatalogService.Infrastructure.Contracts;
+using CatalogService.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 namespace CatalogService.Api
 {
@@ -12,6 +16,8 @@ namespace CatalogService.Api
             string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<CatalogDBContext>(options => options.UseSqlServer(connection));
             builder.Services.AddControllers();
+            builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
+            
             var app = builder.Build();
 
 
