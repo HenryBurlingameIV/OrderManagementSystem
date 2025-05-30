65d50659db073a745b73e51852f29f1996f5ed6d
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-05-30 13:19:10 +0300

    Add ProductRepository

---
+++ CatalogService.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogService.Domain;
+
+namespace CatalogService.Infrastructure.Repositories
+{
+    public class ProductRepository
+    {
+        private CatalogDBContext _dbContext;
+        public ProductRepository(CatalogDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
