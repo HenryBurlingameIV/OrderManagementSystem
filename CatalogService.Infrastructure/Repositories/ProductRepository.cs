178cc1eb3baeaa73d3d0b12c31aadfc1f4a61100
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-05-30 13:19:46 +0300

    Add Contract IRepository<T>

--- CatalogService.Infrastructure/Repositories/ProductRepository.cs
+++ CatalogService.Infrastructure/Repositories/ProductRepository.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CatalogService.Domain;
+using CatalogService.Infrastructure.Contracts;
 
 namespace CatalogService.Infrastructure.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IRepository<Product>
     {
         private CatalogDBContext _dbContext;
         public ProductRepository(CatalogDBContext dbContext)
@@ -15,5 +16,19 @@ namespace CatalogService.Infrastructure.Repositories
             _dbContext = dbContext;
         }
 
+        public Task<Guid> CreateAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> UpdateAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
