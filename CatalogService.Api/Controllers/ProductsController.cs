32fda84ed8b92b369bfb3aceb554fa4bbe885af7
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-01 12:55:34 +0300

    Add constructor in ProductsController

--- CatalogService.Api/Controllers/ProductsController.cs
+++ CatalogService.Api/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CatalogService.Application.Contracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogService.Api.Controllers
@@ -7,6 +8,12 @@ namespace CatalogService.Api.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
 
     }
 }
