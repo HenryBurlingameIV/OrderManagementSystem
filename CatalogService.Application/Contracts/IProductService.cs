175860a705cf88384525b7382c4ed1fc0a666a10
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-01 12:46:05 +0300

    Add CreateProductAsync(),GetProductByIdAsync(),UpdateProductAsync in IProductRepository

--- CatalogService.Application/Contracts/IProductService.cs
+++ CatalogService.Application/Contracts/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CatalogService.Application.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace CatalogService.Application.Contracts
 {
     public interface IProductService
     {
+        Task<Guid> CreateProductAsync(ProductCreateRequest request);
+
+        Task<ProductViewModel> GetProductByIdAsync(Guid productId);
+
+        Task<Guid> UpdateProductAsync(ProductUpdateRequest request);
 
     }
 }
