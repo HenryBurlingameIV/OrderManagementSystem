cbab9bb93edb59531978b0d31cd20d3b0619c1a6
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-10 11:13:09 +0300

    Add CreateAsync_ShouldReturnIdAndSaveProduct()

--- CatalogService.Tests/IntegrationTests/ProductRepositoryIntegrationTests.cs
+++ CatalogService.Tests/IntegrationTests/ProductRepositoryIntegrationTests.cs
@@ -12,25 +12,66 @@ using System.Threading.Tasks;
 
 namespace CatalogService.Tests.IntegrationTests
 {
-    public  class ProductRepositoryIntegrationTests
+    public class ProductRepositoryIntegrationTests : IAsyncLifetime
     {
+        private CatalogDBContext _context;
         private IRepository<Product> _productRepository;
 
-        public ProductRepositoryIntegrationTests()
+        public async Task InitializeAsync()
         {
-            _productRepository = new ProductRepository(GetDBContext());
+            _context = await GetDBContext();
+            _productRepository = new ProductRepository(_context);
         }
 
-        private CatalogDBContext GetDBContext()
+        public Task DisposeAsync()
+        {
+            return _context.DisposeAsync().AsTask();
+        }
+        private async Task<CatalogDBContext> GetDBContext()
         {
             var options = new DbContextOptionsBuilder()
-                .UseInMemoryDatabase("testdb")
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
-            var context =  new CatalogDBContext(options);
+            var context = new CatalogDBContext(options);
             context.Database.EnsureCreated();
 
             return context;
         }
-       
+
+        private Product CreateTestProduct()
+        {
+            return new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "test1",
+                Description = "test1",
+                Category = "test1",
+                Price = 100,
+                Quantity = 1,
+                UpdatedDateUtc = DateTime.UtcNow,
+                CreatedDateUtc = DateTime.UtcNow
+            };
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnIdAndSaveProduct()
+        {
+            //Arrange
+            Product product = CreateTestProduct();
+
+            //Act
+            var actualId = await _productRepository.CreateAsync(product, CancellationToken.None);
+
+            //Assert
+            Assert.Equal(product.Id, actualId);
+            var savedProduct = await _context.Products.FindAsync(product.Id);
+            Assert.NotNull(savedProduct);
+            Assert.Equivalent(product, savedProduct);
+        }
+
+        
     }
+    
+
+
 }
