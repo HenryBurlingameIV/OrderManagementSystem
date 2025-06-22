cc7c7a663552fedb685c68c23ac8ff41a5830d78
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-22 13:02:22 +0300

    Add OrderDbContext

---
+++ OrderService.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions options):base(options)  { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        DbSet<OrderItem> OrderItems { get; set; }
+
+        DbSet<Order> Orders { get; set; }
+    }
+}
