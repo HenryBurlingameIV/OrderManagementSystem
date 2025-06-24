b1f09bf441ebf725e2a3452547472238b1b215fd
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-24 16:22:48 +0300

    Add ProductDto

---
+++ OrderService.Infrastructure/DTO/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.DTO
+{
+    public record ProductDto(Guid Id, decimal Price, int AvailableQuantity);
+}
