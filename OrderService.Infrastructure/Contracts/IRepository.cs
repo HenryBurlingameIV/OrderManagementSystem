4b3f2a66a88ee04af19e749a28b556c7064d83aa
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-24 15:45:26 +0300

    Add IRepository

---
+++ OrderService.Infrastructure/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Contracts
+{
+    public interface IRepository<T>
+    {
+        Task<Guid> CreateAsync(T item, CancellationToken cancellationToken);
+        Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task UpdateAsync(T item, CancellationToken cancellationToken);       
+    }
+}
