9e8e76cda32084df23ed47cc2d0fba677a06c53a
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-25 16:16:49 +0300

    Add OrdersController

---
+++ OrderService.Api/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Application.Commands.CreateOrderCommand;
+
+namespace OrderService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController(IMediator mediator) : ControllerBase
+    {            
+
+
+    }
+}
