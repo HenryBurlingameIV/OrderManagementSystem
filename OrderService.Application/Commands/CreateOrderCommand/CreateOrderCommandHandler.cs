a354eb3796428d6adeb3f3b0b780981200fec21d
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-25 16:09:45 +0300

    Add CreateOrderCommandHandler

--- OrderService.Application/Commands/CreateOrderCommand/CreateOrderCommandHandler.cs
+++ OrderService.Application/Commands/CreateOrderCommand/CreateOrderCommandHandler.cs
@@ -4,15 +4,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OrderService.Infrastructure;
+using OrderService.Infrastructure.Contracts;
+using OrderService.Domain.Entities;
+using OrderService.Application.DTO;
 
 namespace OrderService.Application.Commands.CreateOrderCommand
 {
-    public class CreateOrderCommandHandler() : IRequestHandler<CreateOrderCommand, Guid>
+    public class CreateOrderCommandHandler(OrderDbContext dbContext, ICatalogServiceClient catalogServiceClient) : IRequestHandler<CreateOrderCommand, Guid>
     {
-        public Task<Guid> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        public async Task<Guid> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var orderItemsTasks = request.OrderItems
+                .Select(item => CreateAndReserveItemFromRequest(item, cancellationToken))
+                .ToList();
+
+            var orderItems = (await Task.WhenAll(orderItemsTasks)).ToList();
+
+            var order = CreateOrder(orderItems, DateTime.UtcNow);
+
+            await dbContext.AddAsync(order);
+            await dbContext.SaveChangesAsync();
+            return order.Id;             
         }
 
+        private async Task<OrderItem> CreateAndReserveItemFromRequest(OrderItemRequest request, CancellationToken cancellationToken)
+        {
+            var product = await catalogServiceClient.GetProductByIdAsync(request.Id, cancellationToken);
+            await catalogServiceClient.ReserveProductAsync(request.Id, request.Quantity, cancellationToken);
+            return new OrderItem()
+            {
+                ProductId = request.Id,
+                Quantity = request.Quantity,
+                Price = product.Price,
+            };
+        }
+
+        private Order CreateOrder(List<OrderItem> orderItems, DateTime createdTime)
+        {
+            return new Order()
+            {
+                Id = Guid.NewGuid(),
+                Items = orderItems,
+                TotalPrice = orderItems.Sum(i => i.Price * i.Quantity),
+                Status = OrderStatus.New,
+                CreatedAtUtc = createdTime,
+                UpdatedAtUtc = createdTime
+            };
+        }
     }
 }
