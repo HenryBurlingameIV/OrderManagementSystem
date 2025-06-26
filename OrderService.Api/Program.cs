f60ffab0ab02a93def1ddd9573070f2b651b8d5c
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-06-26 17:45:13 +0300

    Add ConfigureApp and refactoring

--- OrderService.Api/Program.cs
+++ OrderService.Api/Program.cs
@@ -6,6 +6,7 @@ using OrderService.Application.Validators;
 using OrderService.Domain.Entities;
 using OrderService.Infrastructure;
 using OrderService.Infrastructure.Contracts;
+using OrderService.Infrastructure.Extensions;
 using OrderService.Infrastructure.HttpClients;
 using OrderService.Infrastructure.Repositories;
 using Serilog;
@@ -17,39 +18,15 @@ namespace OrderService.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Host.UseSerilog((context, configuration) =>
-            {
-                configuration.ReadFrom.Configuration(context.Configuration);
-            });
-
-            builder.Services.AddMediatR(conf =>
-            {
-                conf.RegisterServicesFromAssembly(typeof(CreateOrderCommand).Assembly);
-            });
-            var connection = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<OrderDbContext>(options => options.UseNpgsql(connection));
-            var catalogConnection = builder.Configuration["CatalogService:DefaultConnection"];
-            builder.Services.AddHttpClient("catalog", c =>
-            {
-                c.BaseAddress = new Uri(catalogConnection!);
-            });
-            builder.Services.AddTransient<ICatalogServiceClient, CatalogServiceClient>();
-            builder.Services.AddScoped<IRepository<Order>, OrderRepository>();
-            builder.Services.AddScoped<IValidator<CreateOrderCommand>, CreateOrderCommandValidator>();
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.ConfigureSerilog();
+            builder.ConfigureServices();
             var app = builder.Build();
+            app.ConfigurePipeline();
 
-            app.UseMiddleware<ExceptionHandlerMiddleware>();
-            app.UseSwagger();
-            app.UseSwaggerUI();
-            app.UseRouting();
-            app.MapControllers();
-
-
-            app.MapGet("/", () => "OrderService is running!");
-
+            if (!app.Environment.IsDevelopment())
+            {
+                app.RunDatabaseMigrations();
+            }
             app.Run();
         }
     }
---
+++ OrderService.Application/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using OrderService.Application.Commands.CreateOrderCommand;
+using OrderService.Application.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Extensions
+{
+    public static class ApplicationExtensions
+    {
+        public static void AddApplication(this IServiceCollection services )
+        {
+            services.AddMediatR(conf =>
+            {
+                conf.RegisterServicesFromAssembly(typeof(CreateOrderCommand).Assembly);
+            });
+
+            services.AddScoped<IValidator<CreateOrderCommand>, CreateOrderCommandValidator>();
+        }
+    }
+}
