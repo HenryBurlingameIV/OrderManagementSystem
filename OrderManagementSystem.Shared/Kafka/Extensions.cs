fed8de8cb8a40f9ab17e512649c1ec0a6f207426
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-04 11:29:46 +0300

    Add Extensions

---
+++ OrderManagementSystem.Shared/Kafka/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderManagementSystem.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Kafka
+{
+    public static class Extensions
+    {
+        public static void AddProducer<TMessage>(this IServiceCollection services, IConfigurationSection config)
+        {
+            services.Configure<KafkaSettings>(config);
+            services.AddSingleton<IKafkaProducer<TMessage>, KafkaProducer<TMessage>>();
+        }
+    }
+}
