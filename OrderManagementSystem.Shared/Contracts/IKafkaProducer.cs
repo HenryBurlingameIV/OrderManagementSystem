0e398ade85d1df1f8b805778829285cdd32e5ffb
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-04 10:40:35 +0300

    Add IKafkaProducer

---
+++ OrderManagementSystem.Shared/Contracts/IKafkaProducer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Contracts
+{
+    public interface IKafkaProducer <in TMessage> : IDisposable
+    {
+        Task ProduceAsync(TMessage message, CancellationToken cancellationToken);
+    }
+}
