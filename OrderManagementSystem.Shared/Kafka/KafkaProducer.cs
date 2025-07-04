bf24f729fb2de2556c5c52c56cad66052d583845
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-04 11:24:52 +0300

    Add KafkaProducer

---
+++ OrderManagementSystem.Shared/Kafka/KafkaProducer.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using OrderManagementSystem.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Kafka
+{
+    public class KafkaProducer<TMessage> : IKafkaProducer<TMessage>
+    {
+        private readonly IProducer<Null, TMessage> _producer;
+        private readonly string _topic;
+        public KafkaProducer(IOptions<KafkaSettings> settings) 
+        {
+            var conf = new ProducerConfig()
+            {
+                BootstrapServers = settings.Value.BootstrapServers,
+            };
+            _producer = new ProducerBuilder<Null, TMessage>(conf)
+                .SetValueSerializer(new KafkaJsonSerializer<TMessage>())
+                .Build();
+
+            _topic = settings.Value.Topic;
+
+        }
+
+        public async Task ProduceAsync(TMessage message, CancellationToken cancellationToken)
+        {
+            await _producer.ProduceAsync(_topic, new Message<Null, TMessage>()
+            {
+                Value = message
+            }, cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            _producer.Dispose();
+        }
+    }
+}
