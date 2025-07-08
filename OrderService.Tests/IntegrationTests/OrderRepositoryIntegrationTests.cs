909f45fa1b7d4e0b88b53c83ba8f31a52d413e08
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-08 14:47:49 +0300

    Add OrderRepositoryIntegrationTests

---
+++ OrderService.Tests/IntegrationTests/OrderRepositoryIntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Tests.IntegrationTests
+{
+    public class OrderRepositoryIntegrationTests : IAsyncLifetime, IClassFixture<OrderRepositoryFixture>
+    {
+        private readonly OrderRepositoryFixture _fixture;
+
+        public OrderRepositoryIntegrationTests(OrderRepositoryFixture fixture) 
+        {
+            _fixture = fixture;
+        }
+        public async Task DisposeAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _fixture.ResetDataBase();
+        }
+    }
+}
