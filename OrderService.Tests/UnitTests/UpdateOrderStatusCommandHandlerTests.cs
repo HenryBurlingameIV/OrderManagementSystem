70d7579b38cbcca1cc9f332a9a29f16ccb82d471
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-09 14:58:58 +0300

    Refactoring UpdateOrderStatusCommandHandlerTests

--- OrderService.Tests/UnitTests/UpdateOrderStatusCommandHandlerTests.cs
+++ OrderService.Tests/UnitTests/UpdateOrderStatusCommandHandlerTests.cs
@@ -43,10 +43,7 @@ namespace OrderService.Tests.UnitTests
             order.Status = from;
             var initialUpdatedAt = order.UpdatedAtUtc;
             var initialTotalPrice = order.TotalPrice;
-
-            var newOrderStatus = to;
-
-            var command = new UpdateOrderStatusCommand(order.Id, newOrderStatus);
+            var command = new UpdateOrderStatusCommand(order.Id, to);
 
             _mockOrderRepository
                 .Setup(repo => repo.GetByIdAsync(
@@ -56,7 +53,7 @@ namespace OrderService.Tests.UnitTests
 
             _mockOrderRepository
                 .Setup(repo => repo.UpdateAsync(
-                    It.Is<Order>(o => o.Id == order.Id && o.Status == newOrderStatus),
+                    It.Is<Order>(o => o.Id == order.Id && o.Status == to),
                     It.IsAny<CancellationToken>()))
                 .Verifiable();
 
@@ -66,9 +63,52 @@ namespace OrderService.Tests.UnitTests
 
             //Assert
             _mockOrderRepository.VerifyAll();
-            Assert.Equal(newOrderStatus, order.Status);
+            Assert.Equal(to, order.Status);
             Assert.True(order.UpdatedAtUtc > initialUpdatedAt);
             Assert.Equal(initialTotalPrice, order.TotalPrice);
+        }
+
+
+        [Theory]
+        [InlineData(OrderStatus.New, OrderStatus.New)]
+        [InlineData(OrderStatus.Processing, OrderStatus.Processing)]
+        [InlineData(OrderStatus.Ready, OrderStatus.Ready)]
+        [InlineData(OrderStatus.Delivering, OrderStatus.Delivering)]
+        [InlineData(OrderStatus.Delivered, OrderStatus.Delivered)]
+        [InlineData(OrderStatus.Cancelled, OrderStatus.Cancelled)]
+        [InlineData(OrderStatus.Cancelled, OrderStatus.New)]
+        [InlineData(OrderStatus.Cancelled, OrderStatus.Ready)]
+        [InlineData(OrderStatus.Cancelled, OrderStatus.Delivering)]
+        [InlineData(OrderStatus.Cancelled, OrderStatus.Delivered)]
+        [InlineData(OrderStatus.Delivered, OrderStatus.New)]
+        [InlineData(OrderStatus.Delivered, OrderStatus.Processing)]
+        [InlineData(OrderStatus.Delivered, OrderStatus.Ready)]
+        [InlineData(OrderStatus.Delivered, OrderStatus.Delivering)]
+        [InlineData(OrderStatus.Delivered, OrderStatus.Cancelled)]
+        public async Task Should_ThrowValidationException_WhenNewStatusIsInvalid(OrderStatus from, OrderStatus to)
+        {
+            //Arrange
+            var order = OrderFactory.CreateSampleOrder(3);
+            order.Status = from;
+            var initialUpdatedAt = order.UpdatedAtUtc;
+            var initialTotalPrice = order.TotalPrice;
+            var command = new UpdateOrderStatusCommand(order.Id, to);
+
+            _mockOrderRepository
+                .Setup(repo => repo.GetByIdAsync(
+                    It.Is<Guid>(id => id == order.Id),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(order);
+
+            _mockOrderRepository
+                .Setup(repo => repo.UpdateAsync(
+                    It.Is<Order>(o => o.Id == order.Id && o.Status == to),
+                    It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act & Assert
+            var exception = await Assert.ThrowsAsync<ValidationException>(() => _handler.Handle(command, CancellationToken.None));
+            Assert.Contains("Cannot change order status from", exception.Message);
         }
 
     }
