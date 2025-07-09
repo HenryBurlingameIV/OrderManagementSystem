d7c5a5e4f2155bd6cd63a76f7f926804c35efddd
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-09 14:03:19 +0300

    Add UpdateOrderStatusCommandHandlerTests

---
+++ OrderService.Tests/UnitTests/UpdateOrderStatusCommandHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Moq;
+using OrderManagementSystem.Shared.Contracts;
+using OrderService.Application.DTO;
+using OrderService.Application.Validators;
+using OrderService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Tests.UnitTests
+{
+    public class UpdateOrderStatusCommandHandlerTests
+    {
+        private IValidator<OrderStatusValidationModel> _validator;
+        private Mock<IRepository<Order>> _mockOrderRepository;
+
+
+        public UpdateOrderStatusCommandHandlerTests()
+        {
+            _validator = new OrderStatusTransitionValidator();
+            _mockOrderRepository = new Mock<IRepository<Order>>();
+        }
+
+        
+    }
+}
