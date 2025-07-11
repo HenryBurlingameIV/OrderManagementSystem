2b51d2ef6c461de39428aeea9f16b7186b045516
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-11 11:13:26 +0300

    Add AutoOrderDataAttribute

---
+++ OrderService.Tests/AutoOrderDataAttribute .cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Tests
+{
+    public class AutoOrderDataAttribute : AutoDataAttribute
+    {
+        public AutoOrderDataAttribute() : base(() => new Fixture().Customize(new OrderFixtureCustomization()))
+        { }
+    }
+}
