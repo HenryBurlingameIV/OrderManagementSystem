f6b6baa311eb50f1c1910d08b18319ea7015282f
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-11 11:04:56 +0300

    Add OrderFixtureCustomization

---
+++ OrderService.Tests/OrderFixtureCustomization .cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Http;
+using OrderService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Tests
+{
+    public class OrderFixtureCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<OrderItem>(composer => composer
+                .With(oi => oi.Price, fixture.Create<decimal>() % 10000 + 1)
+                .With(pi => pi.Quantity, fixture.Create<int>() % 100 + 1)
+            );
+
+            var createdAt = DateTime.UtcNow;
+            fixture.Customize<Order>(composer => composer
+                .FromFactory(() =>
+                {
+                    var order = new Order()
+                    {
+                        Id = Guid.NewGuid(),
+                        CreatedAtUtc = createdAt,
+                        UpdatedAtUtc = createdAt,
+                        Items = fixture
+                            .CreateMany<OrderItem>(fixture.Create<int>() % 10 + 1)
+                            .ToList(),
+                        Status = OrderStatus.New
+                    };
+
+                    order.TotalPrice = order.Items.Sum(oi => oi.Price * oi.Quantity);
+                    return order;
+                }));
+        }
+    }
+}
