ff91e94b9e8f500de3134ee41fa3702b057a8076
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-17 15:24:31 +0300

    Add InfrastructureExtensions

--- OrderProcessingService.Api/Program.cs
+++ OrderProcessingService.Api/Program.cs
@@ -7,6 +7,7 @@ namespace OrderProcessingService.Api
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
+
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
---
+++ OrderProcessingService.Infrastructure/Extensions/InfstractureExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Infrastructure.Extensions
+{
+    public static class InfstractureExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services, string dbconnection)
+        {
+            services.AddDbContext<OrderProcessingDbContext>(options =>
+            {
+                options.UseNpgsql(dbconnection);
+            });
+        }
+    }
+}
