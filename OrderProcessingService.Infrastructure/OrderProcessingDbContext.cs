32d90d5702e20c81c3e878ee6172bc6ec8c36cfb
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-17 14:28:41 +0300

    Add OrderProcessingDbContext

---
+++ OrderProcessingService.Infrastructure/OrderProcessingDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using OrderProcessingService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Infrastructure
+{
+    public class OrderProcessingDbContext : DbContext
+    {
+        public OrderProcessingDbContext(DbContextOptions options) : base (options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        public DbSet<ProcessingOrder> ProcessingOrders { get; set; }       
+    }
+}
