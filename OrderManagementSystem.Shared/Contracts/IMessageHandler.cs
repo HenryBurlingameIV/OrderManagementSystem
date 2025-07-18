9e26f93acd54ef5793f0a3a91fe510dd88f531ca
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-18 11:43:13 +0300

    Add KafkaConsumer

---
+++ OrderManagementSystem.Shared/Contracts/IMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Contracts
+{
+    public interface IMessageHandler<in TMessage>
+    {
+        Task HandleAsync(TMessage message, CancellationToken cancellationToken);
+    }
+}
---
+++ OrderManagementSystem.Shared/Kafka/KafkaConsumer.cs
@@ -0,0 +1,61 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using OrderManagementSystem.Shared.Contracts;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Kafka
+{
+    public class KafkaConsumer<TMessage> : BackgroundService
+    {
+        private readonly string _topic;
+        private readonly IConsumer<string, TMessage> _consumer;
+        private readonly IMessageHandler<TMessage> _handler;
+
+        public KafkaConsumer(IOptions<KafkaConsumerSettings> settings, IMessageHandler<TMessage> handler)
+        {
+            var conf = new ConsumerConfig()
+            {
+                BootstrapServers = settings.Value.BootstrapServers,
+                GroupId = settings.Value.GroupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            _topic = settings.Value.Topic;
+
+            _consumer = new ConsumerBuilder<string, TMessage>(conf)
+                .SetValueDeserializer(new KafkaJsonDeserializer<TMessage>())
+                .Build();
+
+            _handler = handler;
+  
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _consumer.Subscribe(_topic);
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    var result = _consumer.Consume(stoppingToken);
+                    await _handler.HandleAsync(result.Value!, stoppingToken);
+                    _consumer.Commit(result);
+                }
+
+            }
+            catch(OperationCanceledException) 
+            {
+                Log.Information("Consumer stopped");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Unexpected background service error.");
+            }            
+        }
+    }
+}
