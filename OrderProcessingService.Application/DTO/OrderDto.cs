9357977f8e3f235fb93931f300141cd979222958
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-18 13:25:03 +0300

    Add OrderDto, OrderItemDto

---
+++ OrderProcessingService.Application/DTO/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Application.DTO
+{
+    public record OrderDto(
+        Guid Id,
+        List<OrderItemDto> Items,
+        DateTime CreatedAt,
+        DateTime UpdatedAt
+    );
+}
