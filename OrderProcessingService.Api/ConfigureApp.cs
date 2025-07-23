2456b6166fea1bb51bfb12fd602e72508e4f8b99
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-23 15:37:58 +0300

    Add ProcessingOrdersController

--- OrderProcessingService.Api/ConfigureApp.cs
+++ OrderProcessingService.Api/ConfigureApp.cs
@@ -1,4 +1,5 @@
 ﻿using OrderManagementSystem.Shared.Kafka;
+using OrderManagementSystem.Shared.Middlewares;
 using OrderProcessingService.Application.Extensions;
 using OrderProcessingService.Infrastructure.Extensions;
 using Serilog;
@@ -11,7 +12,20 @@ namespace OrderProcessingService.Api
             
             builder.Services.AddApplication();
             builder.Services.AddInfrastructure(builder.Configuration);
-            
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+        }
+
+        public static void ConfigurePipeline(this WebApplication app)
+        {
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseRouting();
+            app.MapControllers();
+            app.MapGet("/", () => "OrderProcessingService is running!");
         }
 
         public static void ConfigureSerilog(this WebApplicationBuilder builder)
---
+++ OrderProcessingService.Api/Controllers/ProcessingOrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using OrderProcessingService.Application.Contracts;
+
+namespace OrderProcessingService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessingOrdersController: ControllerBase
+    {
+        [HttpPatch("{id:Guid}/begin-assembly")]
+        public async Task<ActionResult> BeginAssembly(Guid id, CancellationToken cancellationToken)
+        {
+            return NoContent();
+        }
+
+        [HttpPatch("begin-delivery")]
+        public async Task<ActionResult> BeginDelivery(Guid id, CancellationToken cancellationToken)
+        {
+            return NoContent();
+        }
+    }
+}
