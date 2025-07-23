fbab20b79d94e7fec9c9e3bb157c2d43de255ab0
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-07-23 16:51:24 +0300

    Add OrderProcessor

---
+++ OrderProcessingService.Application/Contracts/IOrderBackgroundWorker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Application.Contracts
+{
+    public interface IOrderBackgroundWorker<TCommand>
+    {
+        Task ScheduleAsync(TCommand command, CancellationToken cancellationToken);
+    }
+
+}
---
+++ OrderProcessingService.Application/Services/OrderProcessor.cs
@@ -0,0 +1,43 @@
+﻿using OrderManagementSystem.Shared.Contracts;
+using OrderManagementSystem.Shared.Exceptions;
+using OrderProcessingService.Application.Contracts;
+using OrderProcessingService.Application.DTO;
+using OrderProcessingService.Domain.Entities;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Application.Services
+{
+    public class OrderProcessor : IOrderProcessor
+    {
+        private readonly IRepository<ProcessingOrder> _repository;
+        private readonly IOrderBackgroundWorker<StartAssemblyCommand> _assemblyWorker;
+
+        public OrderProcessor(IRepository<ProcessingOrder> repository, IOrderBackgroundWorker<StartAssemblyCommand> assemblyWorker)
+        {
+            _repository = repository;
+            _assemblyWorker = assemblyWorker;
+            
+        }
+        public async Task BeginAssembly(Guid id, CancellationToken cancellationToken)
+        {
+            var op = await _repository.GetByIdAsync(id, cancellationToken);
+            if(op is null)
+            {
+                throw new NotFoundException($"Processing order with ID {id} not found.");
+            }
+
+            Log.Information("Processing order with ID {@Id} successfully found", id);
+            await _assemblyWorker.ScheduleAsync(new StartAssemblyCommand(op), cancellationToken);
+        }
+
+        public Task BeginDelivery(List<Guid> ids, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
