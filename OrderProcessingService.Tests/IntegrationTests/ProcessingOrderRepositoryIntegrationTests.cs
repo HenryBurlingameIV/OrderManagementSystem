4151d8b96f74cc5ea38df01cc597762feb3ff674
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-03 13:50:52 +0300

    Add ProcessingOrderRepositoryIntegrationTests class

---
+++ OrderProcessingService.Tests/IntegrationTests/ProcessingOrderRepositoryIntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using OrderProcessingService.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Tests.IntegrationTests
+{
+    public class ProcessingOrderRepositoryIntegrationTests : IClassFixture<ProcessingOrderRepositoryFixture>, IAsyncLifetime
+    {
+        private readonly ProcessingOrderRepositoryFixture _fixture;
+
+        public ProcessingOrderRepositoryIntegrationTests(ProcessingOrderRepositoryFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _fixture.ResetDataBase();
+        }
+    }
+}
