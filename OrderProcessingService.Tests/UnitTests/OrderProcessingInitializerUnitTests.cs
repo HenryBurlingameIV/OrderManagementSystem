10ddd1d1a381aa525ce4de01387ac467eedbfc72
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-04 11:18:13 +0300

    Add OrderProcessingInitializerUnitTests

---
+++ OrderProcessingService.Tests/UnitTests/OrderProcessingInitializerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using OrderManagementSystem.Shared.Contracts;
+using OrderProcessingService.Application.Services;
+using OrderProcessingService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Tests.UnitTests
+{
+    public class OrderProcessingInitializerUnitTests
+    {
+        private readonly Mock<IRepository<ProcessingOrder>> _mockRepository;
+        private readonly OrderProcessingInitializer _orderProcessingInitializer;
+
+        public OrderProcessingInitializerUnitTests() 
+        {
+            _mockRepository = new Mock<IRepository<ProcessingOrder>>();
+            _orderProcessingInitializer = new OrderProcessingInitializer(_mockRepository.Object);
+        }
+    }
+}
