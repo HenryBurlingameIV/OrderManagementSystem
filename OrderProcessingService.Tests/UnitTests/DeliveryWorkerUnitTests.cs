d8beb8d8e8d48469f10ac9c42ccdebdf0ee31c03
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-06 12:23:47 +0300

    Add DeliveryWorkerUnitTests

---
+++ OrderProcessingService.Tests/UnitTests/DeliveryWorkerUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OrderProcessingService.Application.Contracts;
+using OrderProcessingService.Infrastructure.BackgroundWorkers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingService.Tests.UnitTests
+{
+    public class DeliveryWorkerUnitTests
+    {
+        private readonly Mock<IProcessingOrderRepository> _mockRepository;
+        private readonly Mock<IBackgroundJobClient> _mockBackgroundJobClient;
+        private readonly Mock<IOrderServiceApi> _mockOrderServiceApi;
+        private readonly Mock<ILogger<DeliveryWorker>> _mockLogger;
+        private readonly DeliveryWorker _deliveryWorker;
+
+        public DeliveryWorkerUnitTests()
+        {
+            _mockRepository = new Mock<IProcessingOrderRepository>();
+            _mockBackgroundJobClient = new Mock<IBackgroundJobClient>();
+            _mockOrderServiceApi = new Mock<IOrderServiceApi>();
+            _mockLogger = new Mock<ILogger<DeliveryWorker>>();
+            _deliveryWorker = new DeliveryWorker(
+                _mockBackgroundJobClient.Object,
+                _mockRepository.Object,
+                _mockOrderServiceApi.Object,
+                _mockLogger.Object
+                );
+        }
+    }
+}
