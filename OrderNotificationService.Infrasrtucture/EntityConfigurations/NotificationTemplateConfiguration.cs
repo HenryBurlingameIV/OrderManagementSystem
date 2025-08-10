9de34edd68eafb19eaade356c49c38734d9b52f9
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-10 13:04:34 +0300

    Add NotificationTemplateConfiguration

---
+++ OrderNotificationService.Infrasrtucture/EntityConfigurations/NotificationTemplateConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderNotificationService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNotificationService.Infrasrtucture.EntityConfigurations
+{
+    public class NotificationTemplateConfiguration : IEntityTypeConfiguration<NotificationTemplate>
+    {
+        public void Configure(EntityTypeBuilder<NotificationTemplate> builder)
+        {
+            builder
+                .HasKey(n => n.Id);
+
+            builder
+                .Property(n => n.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(n => n.TemplateText)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
