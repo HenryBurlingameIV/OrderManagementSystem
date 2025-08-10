75608696224ce3b9150f3e53112cd3b87c0d72a4
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-10 12:31:24 +0300

    Add OrderNotificationDbContext

---
+++ OrderNotificationService.Infrasrtucture/OrderNotificationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderNotificationService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNotificationService.Infrasrtucture
+{
+    public class OrderNotificationDbContext : DbContext
+    {
+        public OrderNotificationDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<NotificationTemplate> NotificationTemplates { get; set; }
+
+
+    }
+}
