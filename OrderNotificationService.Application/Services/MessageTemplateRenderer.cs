42e10f73c8604bc2f4487b5a0364d1f9bd794148
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-18 15:05:38 +0300

    Add MessageTemplateRenderer

---
+++ OrderNotificationService.Application/Services/MessageTemplateRenderer.cs
@@ -0,0 +1,21 @@
+﻿using OrderNotificationService.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNotificationService.Application.Services
+{
+    public class MessageTemplateRenderer : IMessageTemplateRenderer
+    {
+        public string Render(string template, object values)
+        {
+            foreach (var property in values.GetType().GetProperties())
+            {
+                template = template.Replace($@"{{{property.Name}}}", property?.GetValue(values)?.ToString() ?? "");
+            }
+            return template;
+        }
+    }
+}
