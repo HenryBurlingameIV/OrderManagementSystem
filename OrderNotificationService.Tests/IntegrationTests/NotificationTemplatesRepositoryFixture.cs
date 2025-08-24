94133038ff993ff7e4f2e8424ac1cb7d05186339
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-24 11:40:36 +0300

    Add Should_ReturnNotificationTemplate_WhenExists()

--- OrderNotificationService.Tests/IntegrationTests/NotificationTemplatesRepositoryFixture.cs
+++ OrderNotificationService.Tests/IntegrationTests/NotificationTemplatesRepositoryFixture.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OrderManagementSystem.Shared.Enums;
 using OrderNotificationService.Application.Contracts;
+using OrderNotificationService.Domain.Entities;
 using OrderNotificationService.Infrastructure;
 using OrderNotificationService.Infrastructure.Repositories;
 using System;
@@ -12,21 +14,63 @@ namespace OrderNotificationService.Tests.IntegrationTests
 {
     public class NotificationTemplatesRepositoryFixture : IDisposable
     {
-        private readonly OrderNotificationDbContext _orderNotificationDbContext;
-        private readonly INotificationTemplatesRepository _notificationTemplatesRepository;
+        public OrderNotificationDbContext OrderNotificationDbContext { get ; private set; }
+        public INotificationTemplatesRepository NotificationTemplatesRepository { get; private set; }
 
         public NotificationTemplatesRepositoryFixture()
         {
-            var options =  new DbContextOptionsBuilder()
+            var options =  new DbContextOptionsBuilder<OrderNotificationDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
-            _orderNotificationDbContext = new OrderNotificationDbContext(options);
-            _notificationTemplatesRepository = new NotificationTemplatesRepository(_orderNotificationDbContext);
+            OrderNotificationDbContext = new OrderNotificationDbContext(options);
+            NotificationTemplatesRepository = new NotificationTemplatesRepository(OrderNotificationDbContext);
+            var templates = new List<NotificationTemplate>()
+            {
+                 new NotificationTemplate()
+                {
+                    Id = (int)OrderStatus.New,
+                    Name = "Создан",
+                    TemplateText = "Ваш заказ {OrderId} создан."
+                },
+                new NotificationTemplate()
+                {
+                    Id = (int)OrderStatus.Cancelled,
+                    Name = "Отменен",
+                    TemplateText = "Ваш заказ {OrderId} отменен."
+                },
+                new NotificationTemplate()
+                {
+                    Id = (int)OrderStatus.Processing,
+                    Name = "В обработке",
+                    TemplateText = "Ваш заказ {OrderId} находится в обработке."
+                },
+                new NotificationTemplate()
+                {
+                    Id = (int)OrderStatus.Ready,
+                    Name = "Готов к доставке",
+                    TemplateText = "Обработка вашего заказа {OrderId} завершена. Заказ готов к доставке."
+                },
+                new NotificationTemplate()
+                {
+                    Id = (int)OrderStatus.Delivering,
+                    Name = "В доставке",
+                    TemplateText = "Ваш заказ {OrderId} находится в процессе доставки."
+                },
+                new NotificationTemplate()
+                {
+                    Id = (int)OrderStatus.Delivered,
+                    Name = "Доставлен",
+                    TemplateText = "Ваш заказ {OrderId} доставлен по указанному адресу."
+                }
+            };
+            OrderNotificationDbContext.NotificationTemplates.AddRange(templates);
+            OrderNotificationDbContext.SaveChanges();
         }
 
         public void Dispose()
         {
-            _orderNotificationDbContext.Dispose();
+            OrderNotificationDbContext.Database.EnsureDeleted();
+            OrderNotificationDbContext.Dispose();
         }
     }
 }
--- OrderNotificationService.Tests/IntegrationTests/NotificationTemplatesRepositoryIntegrationTests.cs
+++ OrderNotificationService.Tests/IntegrationTests/NotificationTemplatesRepositoryIntegrationTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderManagementSystem.Shared.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,33 @@ namespace OrderNotificationService.Tests.IntegrationTests
             _fixture = fixture;
         }
 
+        [Theory]
+        [InlineData(OrderStatus.New, "Ваш заказ {OrderId} создан.")]
+        [InlineData(OrderStatus.Cancelled, "Ваш заказ {OrderId} отменен.")]
+        [InlineData(OrderStatus.Processing, "Ваш заказ {OrderId} находится в обработке.")]
+        [InlineData(OrderStatus.Ready, "Обработка вашего заказа {OrderId} завершена. Заказ готов к доставке.")]
+        [InlineData(OrderStatus.Delivering, "Ваш заказ {OrderId} находится в процессе доставки.")]
+        [InlineData(OrderStatus.Delivered, "Ваш заказ {OrderId} доставлен по указанному адресу.")]
+        public async Task Should_ReturnNotificationTemplate_WhenExists(OrderStatus status, string expectedTemplateText)
+        {
+            //Act
+            var actualTemplate = await _fixture.NotificationTemplatesRepository.GetNotificationTemplateByIdAsync((int)status, CancellationToken.None);
 
+            //Assert
+            Assert.NotNull(actualTemplate);
+            Assert.Equal(expectedTemplateText, actualTemplate?.TemplateText);
+        }
+
+        [Fact]
+        public async Task Should_ReturnNull_WhenTemplateNotFound()
+        {
+            //Act
+            var actualTemplate = await _fixture.NotificationTemplatesRepository.GetNotificationTemplateByIdAsync(10, CancellationToken.None);
+
+            //Assert
+            Assert.Null(actualTemplate);
+        }
     }
+
 }
+
