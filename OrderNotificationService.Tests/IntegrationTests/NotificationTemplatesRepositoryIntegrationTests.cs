4b116434af5b72c112001351489819a5335982d0
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-24 11:16:09 +0300

    Add NotificationTemplatesRepositoryIntegrationTests

---
+++ OrderNotificationService.Tests/IntegrationTests/NotificationTemplatesRepositoryIntegrationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNotificationService.Tests.IntegrationTests
+{
+    public class NotificationTemplatesRepositoryIntegrationTests : IClassFixture<NotificationTemplatesRepositoryFixture>
+    {
+        private readonly NotificationTemplatesRepositoryFixture _fixture;
+
+        public NotificationTemplatesRepositoryIntegrationTests(NotificationTemplatesRepositoryFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+
+    }
+}
