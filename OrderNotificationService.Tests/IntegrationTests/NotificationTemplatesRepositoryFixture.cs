c745d0c9977b637bf02ba29fa3542763f1cc9302
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-24 11:13:21 +0300

    Add NotificationTemplatesRepositoryFixture

---
+++ OrderNotificationService.Tests/IntegrationTests/NotificationTemplatesRepositoryFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderNotificationService.Application.Contracts;
+using OrderNotificationService.Infrastructure;
+using OrderNotificationService.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderNotificationService.Tests.IntegrationTests
+{
+    public class NotificationTemplatesRepositoryFixture : IDisposable
+    {
+        private readonly OrderNotificationDbContext _orderNotificationDbContext;
+        private readonly INotificationTemplatesRepository _notificationTemplatesRepository;
+
+        public NotificationTemplatesRepositoryFixture()
+        {
+            var options =  new DbContextOptionsBuilder()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _orderNotificationDbContext = new OrderNotificationDbContext(options);
+            _notificationTemplatesRepository = new NotificationTemplatesRepository(_orderNotificationDbContext);
+        }
+
+        public void Dispose()
+        {
+            _orderNotificationDbContext.Dispose();
+        }
+    }
+}
