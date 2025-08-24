94c1eef704db98c82a36c3da0eec464f90f67f5f
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-08-24 13:14:24 +0300

    Add Should_Notify_WhenTemplateExists()

--- OrderNotificationService.Tests/UnitTests/NotificationServiceUnitTests.cs
+++ OrderNotificationService.Tests/UnitTests/NotificationServiceUnitTests.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Logging;
 using Moq;
 using OrderNotificationService.Application.Contracts;
+using OrderNotificationService.Application.DTO;
 using OrderNotificationService.Application.Services;
+using OrderNotificationService.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +32,48 @@ namespace OrderNotificationService.Tests.UnitTests
                 _mockEmailMessageSender.Object,
                 _mockMessageTemplateRenderer.Object,
                 _mockLogger.Object);
+        }
+
+        [Fact]
+        public async Task Should_Notify_WhenTemplateExists()
+        {
+            //Arrange
+            var orderId = Guid.NewGuid();
+            var email = "test@gmail.com";
+            var orderStatus = 1;
+            var request = new NotificationRequest(orderId, orderStatus, email);
+            var templateText = "Ваш заказ {OrderId} создан.";
+            var expectedMessage = $"Ваш заказ {orderId} создан.";
+            var template = new NotificationTemplate
+            {
+                Id = orderStatus,
+                Name = "Создан",
+                TemplateText = templateText
+            };
+
+            _mockRepository
+                .Setup(repo => repo.GetNotificationTemplateByIdAsync(orderStatus, CancellationToken.None))
+                .ReturnsAsync(template);
+      
+
+            _mockEmailMessageSender
+                .Setup(sender => sender.SendAsync(expectedMessage, email, It.IsAny<CancellationToken>()));
+
+            _mockMessageTemplateRenderer
+                .Setup(renderer => renderer
+                    .Render(
+                        templateText, 
+                        It.Is<Dictionary<string, string>>(d =>
+                            d.ContainsKey("OrderId") && d["OrderId"] == orderId.ToString())))
+                .Returns(expectedMessage);
+
+            //Act
+            await _notificationService.NotifyAsync(request, CancellationToken.None);
+
+            //Assert
+            _mockRepository.VerifyAll();
+            _mockEmailMessageSender.VerifyAll();
+            _mockMessageTemplateRenderer.VerifyAll();               
         }
     }
 }
