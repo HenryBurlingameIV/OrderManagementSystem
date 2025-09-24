364862d22fcafdb2c8777703e8d0b01f834eebdf
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-09-24 16:29:31 +0300

    Add YARP

---
+++ OrderManagementSystem.Gateway/Program.cs
@@ -0,0 +1,18 @@
+namespace OrderManagementSystem.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("YARP"));
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+            app.MapReverseProxy();
+
+            app.Run();
+        }
+    }
+}
