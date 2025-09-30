6fecc53d35212d475a961a91a91452b0815d4de5
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-09-30 16:17:03 +0300

    Edit ProductRequests

--- CatalogService.Application/DTO/ProductUpdateRequest.cs
+++ CatalogService.Application/DTO/ProductUpdateRequest.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace CatalogService.Application.DTO
 {
-    public class ProductUpdateRequest
-    {
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public string? Category { get; set; }
-        public decimal? Price { get; set; }
-        public int? Quantity { get; set; }
-    }
+    public record ProductUpdateRequest(string? Name, string? Description, string? Category, decimal? Price, int? Quantity);
+
 }
