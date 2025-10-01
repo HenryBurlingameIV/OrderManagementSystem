71d19efeb21d230513db7f0a07be3bb30a0bcc28
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-10-01 14:32:27 +0300

    Add GetProductsRequest

---
+++ CatalogService.Application/DTO/GetProductsRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogService.Application.DTO
+{
+    public record GetProductsRequest(int PageNumber, int PageSize, string? Search, string? SortBy);
+
+}
