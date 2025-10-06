468d4ad3d17a73635feb33bc7b0af825406fed56
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-10-06 13:02:03 +0300

    Add IRepositoryBase

---
+++ OrderManagementSystem.Shared/Contracts/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Contracts
+{
+    public interface IRepositoryBase<TEntity, in TKey> where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(TKey id, CancellationToken cancellationToken);
+        Task<TEntity> InsertAsync(TEntity item, CancellationToken cancellationToken);
+
+        void Update(TEntity item);
+        void Delete(TEntity item);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
