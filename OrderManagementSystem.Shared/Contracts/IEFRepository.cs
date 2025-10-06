7cfa9b8f729318363d9ec581d186342f891cbf18
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-10-06 13:02:30 +0300

    Add IEFRepository

---
+++ OrderManagementSystem.Shared/Contracts/IEFRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using OrderManagementSystem.Shared.DataAccess.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Contracts
+{
+    public interface IEFRepository<TEntity, TKey> : IRepositoryBase<TEntity, TKey>  where TEntity : class
+    {
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct);
+        Task<TEntity?> FindAsync(object[] keyValues, CancellationToken ct);
+
+        Task<PaginatedResult<TResult>> GetPaginated<TResult>(
+            PaginationRequest request,
+            Expression<Func<TEntity, TResult>> selector,
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            bool asNoTracking = true,
+            CancellationToken ct = default) where TResult : class;
+
+        Task<PaginatedResult<TEntity>> GetPaginated(
+            PaginationRequest request,
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            bool asNoTracking = true,
+            CancellationToken ct = default);
+
+        Task<TEntity?> GetFirstOrDefaultAsync(
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            bool asNoTraсking = true,
+            CancellationToken ct = default);
+
+        Task<TResult?> GetFirstOrDefaultAsync<TResult>(
+            Expression<Func<TEntity, TResult>> selector,
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            bool asNoTraсking = true,
+            CancellationToken ct = default);
+
+    }
+}
