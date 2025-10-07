7e241c33c1971b82e268c23096342423e3e5ccb7
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-10-07 16:57:40 +0300

    Add GetPaginatedOrdersQuery

---
+++ OrderService.Application/Queries/OrdersPaginatedQuery/GetPaginatedOrdersQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using OrderManagementSystem.Shared.DataAccess.Pagination;
+using OrderService.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Queries.OrdersPaginatedQuery
+{
+    public record GetPaginatedOrdersQuery(GetPaginatedOrdersRequest Request) : IRequest<PaginatedResult<OrderViewModel>>;
+
+}
