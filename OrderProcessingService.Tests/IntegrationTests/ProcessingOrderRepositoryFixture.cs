b03aa6388bd3e7137066a184e7de775250e75310
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-10-13 13:12:05 +0300

    Edit ProcessingOrderRepositoryFixture

--- OrderProcessingService.Tests/IntegrationTests/ProcessingOrderRepositoryFixture.cs
+++ OrderProcessingService.Tests/IntegrationTests/ProcessingOrderRepositoryFixture.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OrderManagementSystem.Shared.Contracts;
+using OrderManagementSystem.Shared.DataAccess;
+using OrderProcessingService.Domain.Entities;
 using OrderProcessingService.Infrastructure;
 using OrderProcessingService.Infrastructure.Repositories;
 using System;
@@ -12,7 +15,9 @@ namespace OrderProcessingService.Tests.IntegrationTests
     public class ProcessingOrderRepositoryFixture : IAsyncLifetime
     {
         public OrderProcessingDbContext DbContext { get; private set; }
-        public ProcessingOrderRepository ProcessingOrderRepository { get; private set; }
+        public IEFRepository<ProcessingOrder,Guid> ProcessingOrdersRepository { get; private set; }
+
+        public IEFRepository<OrderItem, Guid> OrderItemsRepository { get; private set; }
 
         public async Task InitializeAsync()
         {
@@ -22,7 +27,8 @@ namespace OrderProcessingService.Tests.IntegrationTests
 
             DbContext = new OrderProcessingDbContext(options);
 
-            ProcessingOrderRepository = new ProcessingOrderRepository(DbContext);
+            ProcessingOrdersRepository = new Repository<ProcessingOrder, Guid>(DbContext);
+            OrderItemsRepository = new Repository<OrderItem, Guid>(DbContext);
             await DbContext.Database.EnsureCreatedAsync();
         }
 
