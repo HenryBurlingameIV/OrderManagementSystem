15fd479d1b0431a24893edd648366e6e0aa0fdb7
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-10-22 16:43:36 +0300

    Add CatalogGrpcServiceApi

---
+++ OrderService.Infrastructure/ExternalServices/CatalogGrpcServiceApi.cs
@@ -0,0 +1,49 @@
+﻿using OrderService.Application.Contracts;
+using OrderService.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogService.Api.Protos;
+
+namespace OrderService.Infrastructure.ExternalServices
+{
+    public class CatalogGrpcServiceApi : ICatalogServiceApi
+    {
+        private readonly Catalog.CatalogClient _client;
+
+        public CatalogGrpcServiceApi(Catalog.CatalogClient client)
+        {
+            _client = client;
+        }
+        public async Task ReleaseProductAsync(Guid id, int quantity, CancellationToken cancellationToken)
+        {
+            var request = new ReleaseProductRequest()
+            {
+                ProductId = id.ToString(),
+                Quantity = quantity,
+            };
+
+
+            await _client.ReleaseProductAsync(request, cancellationToken: cancellationToken);
+        }
+
+        public async Task<ProductDto?> ReserveProductAsync(Guid id, int quantity, CancellationToken cancellationToken)
+        {
+            var request = new ReserveProductRequest()
+            {
+                ProductId = id.ToString(),
+                Quantity = quantity,
+            };
+
+
+            var response = await _client.ReserveProductAsync(request, cancellationToken: cancellationToken);
+
+            return new ProductDto(
+                Guid.Parse(response.ProductId),
+                decimal.Parse(response.Price),
+                quantity);
+        }
+    }
+}
