5c1a98778a16b84d57cb126a4f12039135d6db13
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-11-01 13:24:17 +0300

    Add Entities

---
+++ AuthService.Domain/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+
+        public List<Role> Roles { get; set; } = new();
+
+        public List<string> GetAllPermissions()
+        {
+            return Roles?
+                .SelectMany(r => r.Permissions)
+                .Select(p => p.Name)
+                .Distinct()
+                .ToList() ?? new List<string>();
+        }
+    }
+}
