e5e532950ab3393be214868873e5d505412ce529
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-11-02 16:55:17 +0300

    Add JwtProvider

---
+++ AuthService.Infrastructure/Services/JwtProvider.cs
@@ -0,0 +1,49 @@
+﻿using AuthService.Application;
+using AuthService.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using OrderManagementSystem.Shared.Authorization;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Infrastructure.Services
+{
+    public class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtProvider(IOptions<JwtOptions> options)
+        {
+            _jwtOptions = options.Value;
+        }
+        public string GenerateToken(User user)
+        {
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Name, user.Name),
+                new Claim(CustomClaimTypes.Roles, string.Join(",", user.Roles.Select(r => r.Name))),
+                new Claim (CustomClaimTypes.Permissions, string.Join(",", user.GetAllPermissions()))
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.Add(_jwtOptions.Expires),
+                claims: claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
