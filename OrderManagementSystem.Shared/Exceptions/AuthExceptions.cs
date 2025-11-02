6436259483cc7233a08b3b1c9dbbca81672007d1
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-11-02 16:56:34 +0300

    Add AuthExceptions

---
+++ OrderManagementSystem.Shared/Exceptions/AuthExceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Exceptions
+{
+    public class AuthExceptions
+    {
+        public class AuthenticationException : Exception
+        {
+            public AuthenticationException(string message) : base(message) { }
+        }
+
+        public class InvalidCredentialsException : AuthenticationException
+        {
+            public InvalidCredentialsException() : base("Invalid email or password") { }
+        }
+
+        public class AccountInactiveException : AuthenticationException
+        {
+            public AccountInactiveException() : base("Account is deactivated") { }
+        }
+
+        public class ForbiddenException : AuthenticationException
+        {
+            public ForbiddenException() : base("Access forbidden") { }
+
+        }
+    }
+}
