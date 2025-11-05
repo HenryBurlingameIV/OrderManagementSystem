a6aa221a3405e7c10b2f47d3b5b32c0edf1897cf
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-11-05 16:30:11 +0300

    Add RoleProvider

---
+++ AuthService.Application/Contracts/IRoleProvider.cs
@@ -0,0 +1,17 @@
+﻿using AuthService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Application.Contracts
+{
+    public interface IRoleProvider
+    {
+        Task<Role> GetClientRoleAsync(CancellationToken ct);
+        Task<Role> GetAdminRoleAsync(CancellationToken ct);
+        Task<Role> GetManagerRoleAsync(CancellationToken ct);
+        Task<IList<Role>> GetRolesByNamesAsync(List<string> roleNames, CancellationToken ct);
+    }
+}
---
+++ AuthService.Application/Services/RoleProvider.cs
@@ -0,0 +1,48 @@
+﻿using AuthService.Application.Contracts;
+using AuthService.Domain.Entities;
+using OrderManagementSystem.Shared.Authorization;
+using OrderManagementSystem.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Application.Services
+{
+    public class RoleProvider : IRoleProvider
+    {
+        private readonly IEFRepository<Role, int> _rolesRepository;
+
+        public RoleProvider(IEFRepository<Role, int> rolesRepository)
+        {
+            _rolesRepository = rolesRepository;
+        }
+        public async Task<Role> GetAdminRoleAsync(CancellationToken ct) =>
+            await _rolesRepository.GetFirstOrDefaultAsync(
+                filter: r => r.Name == Roles.Admin,
+                asNoTraсking: false,
+                ct: ct) ?? throw new InvalidOperationException("Admin role not found.");
+
+
+        public async Task<Role> GetClientRoleAsync(CancellationToken ct) =>        
+            await _rolesRepository.GetFirstOrDefaultAsync(
+                filter: r => r.Name == Roles.Client,
+                asNoTraсking: false,
+                ct: ct) ?? throw new InvalidOperationException("Client role not found.");
+        
+
+        public async Task<Role> GetManagerRoleAsync(CancellationToken ct) =>
+            await _rolesRepository.GetFirstOrDefaultAsync(
+                filter: r => r.Name == Roles.Manager,
+                asNoTraсking: false,
+                ct: ct) ?? throw new InvalidOperationException("Admin role not found.");
+
+
+        public async Task<IList<Role>> GetRolesByNamesAsync(List<string> roleNames, CancellationToken ct) =>
+            await _rolesRepository.GetAllAsync(
+                filter: r => roleNames.Contains(r.Name),
+                asNoTraсking: false,
+                ct: ct);
+    }
+}
