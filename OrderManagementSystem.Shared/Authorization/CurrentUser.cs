426d31bb3f305ad9949e04b433e1a806d3f02e86
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-11-06 16:51:07 +0300

    Add CurrentUser

---
+++ OrderManagementSystem.Shared/Authorization/CurrentUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using OrderManagementSystem.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Authorization
+{
+    public class CurrentUser : ICurrentUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public Guid UserId
+        {
+            get
+            {
+                var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                return Guid.TryParse(userId, out var result) ? result : Guid.Empty;
+            }
+        }
+        public string Email => _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value ?? "";
+
+    }
+}
---
+++ OrderManagementSystem.Shared/Contracts/ICurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Shared.Contracts
+{
+    public interface ICurrentUser
+    {
+        Guid UserId { get; }
+        string Email {  get; }
+    }
+}
