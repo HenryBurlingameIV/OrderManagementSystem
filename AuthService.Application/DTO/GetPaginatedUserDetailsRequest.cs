0ecd0a8b40cc5548d5dd41227a254efb7c3517b6
Михаил Ивлиев <mr.ivliev@gmail.com>
2025-11-11 16:06:04 +0300

    Add GetPaginatedUserDetailsRequest

---
+++ AuthService.Application/DTO/GetPaginatedUserDetailsRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Application.DTO
+{
+    public record GetPaginatedUserDetailsRequest(
+        int PageNumber,
+        int PageSize,
+        string? Search = null,          
+        List<string>? Roles = null,     
+        bool? IsActive = null,          
+        string? SortBy = "Email",
+        bool Descending = false
+    );
+
+}
